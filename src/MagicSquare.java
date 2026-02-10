7bdc07514c3ef14578c2a97e9e5c843ef30ea8e5
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-02-10 08:14:14 -0500

    Implement magic square checks and sum methods
    Added methods for checking magic square properties and calculating sums.

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,29 +1,109 @@
-
 public class MagicSquare {
-	public static void main(String[] args) {
-		int[][] square1 = { { 10, 3, 8 }, { 5, 7, 9 }, { 6, 11, 4 } };
-		int[][] square2 = { { 10, 3, 8 }, { 5, 7, 9 }, { 6, 11, 3 } };
-		int[][] square3 = { { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } };
-		int[][] square4 = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
-		int[][] square5 = { { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 9 } };
-		int[][] square6 = { { 5, 1, 1, 1 }, { 1, 5, 1, 1 }, { 1, 1, 5, 1 }, { 1, 1, 1, 5 } };
 
-		System.out.println("Test 1: " + isMagic(square1));
-		System.out.println("Test 2: " + isMagic(square2));
-		System.out.println("Test 3: " + isMagic(square3));
-		System.out.println("Test 4: " + isMagic(square4));
-		System.out.println("Test 5: " + isMagic(square5));
-		System.out.println("Test 6: " + isMagic(square6));
+    /**
+     * Returns true if the 2D array is a magic square.
+     *
+     * Algorithm:
+     * 1. Compute the sum of the first row and store it as the target sum.
+     * 2. Check that every remaining row has the same sum.
+     *    - If any row sum is different, return false.
+     * 3. Check that every column has the same sum.
+     *    - If any column sum is different, return false.
+     * 4. Check that both diagonals have the same sum as the target.
+     *    - If either diagonal is different, return false.
+     * 5. If all checks pass, return true.
+     */
+    public static boolean isMagic(int[][] arr) {
+        // TODO: Student implementation
+        return false;
+    }
 
-	}
+    /**
+     * Returns the sum of all values in the given row.
+     * Precondition: row is a valid index in arr.
+     */
+    public static int rowSum(int[][] arr, int row) {
+        // TODO: Student implementation
+        return 0;
+    }
 
-	// POST: returns true if the array is magic and
-	// false otherwise
-	public static boolean isMagic(int[][] arr) {
-		// Your code here
-	}
+    /**
+     * Returns the sum of all values in the given column.
+     * Precondition: col is a valid index in arr.
+     */
+    public static int colSum(int[][] arr, int col) {
+        // TODO: Student implementation
+        return 0;
+    }
 
-	// Copy rowSum here
+    /**
+     * Returns the sum of the main diagonal (top-left to bottom-right).
+     */
+    public static int mainDiagonalSum(int[][] arr) {
+        // TODO: Student implementation
+        return 0;
+    }
 
-	// Copy colSum here
+    /**
+     * Returns the sum of the secondary diagonal (top-right to bottom-left).
+     */
+    public static int secondaryDiagonalSum(int[][] arr) {
+        // TODO: Student implementation
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] square3x3 = {
+            {2, 7, 6},
+            {9, 5, 1},
+            {4, 3, 8}
+        };
+
+        int[][] square4x4 = {
+            {16, 2, 3, 13},
+            {5, 11, 10, 8},
+            {9, 7, 6, 12},
+            {4, 14, 15, 1}
+        };
+
+        int[][] square5x5 = {
+            {17, 24, 1, 8, 15},
+            {23, 5, 7, 14, 16},
+            {4, 6, 13, 20, 22},
+            {10, 12, 19, 21, 3},
+            {11, 18, 25, 2, 9}
+        };
+
+        int[][] notMagic = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        // ---- Helper Method Tests (2 per method) ----
+
+        // rowSum tests
+        System.out.println("Row 0 sum (3x3): " + rowSum(square3x3, 0));
+        System.out.println("Row 2 sum (5x5): " + rowSum(square5x5, 2));
+
+        // colSum tests
+        System.out.println("Col 1 sum (3x3): " + colSum(square3x3, 1));
+        System.out.println("Col 3 sum (4x4): " + colSum(square4x4, 3));
+
+        // main diagonal tests
+        System.out.println("Main diagonal sum (3x3): " + mainDiagonalSum(square3x3));
+        System.out.println("Main diagonal sum (4x4): " + mainDiagonalSum(square4x4));
+
+        // secondary diagonal tests
+        System.out.println("Secondary diagonal sum (3x3): " + secondaryDiagonalSum(square3x3));
+        System.out.println("Secondary diagonal sum (5x5): " + secondaryDiagonalSum(square5x5));
+
+        // ---- Magic Square Tests ----
+        System.out.println("3x3 magic (expected true):  " + isMagic(square3x3));
+        System.out.println("4x4 magic (expected true):  " + isMagic(square4x4));
+        System.out.println("5x5 magic (expected true):  " + isMagic(square5x5));
+        System.out.println("Not magic (expected false): " + isMagic(notMagic));
+    }
 }
